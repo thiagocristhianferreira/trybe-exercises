02a9bc0c50702a339eeae89b846b86e248487b8a
Thiago Ferreira <thiagocristhian@gmail.com>
2020-10-24 18:25:33 -0300

    exercicios 4.1 - JavaScript - Primeiros PAssos

---
+++ modulo1_fundamentals/bloco_4/exerc7_porcentagem.js
@@ -0,0 +1,16 @@
+let nota=86;
+if (nota>100 || nota<0) {
+    console.log("Nota Inválida");
+} else if(nota>=90) {
+    console.log("Nota A");
+} else if(nota>=80) {
+    console.log("Nota B");
+} else if(nota>=70) {
+    console.log("Nota C");
+} else if(nota>=60) {
+    console.log("Nota D");
+} else if(nota>=50) {
+    console.log("Nota E");
+} else if(nota<50) {
+    console.log("Nota F");
+}
